61c1db9a09caa0f918f8d14675089b3d988fcc15
aaronkyle1115 <44892090+aaronkyle1115@users.noreply.github.com>
2019-02-11 14:05:45 +0800

    Add files via upload

---
+++ Rpg/Archer.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpg;
+
+/**
+ *
+ * @author m304user
+ */
+public class Archer  extends Character{
+    
+private int maxmagic, currentmagic;
+    public Archer(String name, int str, int dex, int intel){
+        super.Name = name;
+        super.Strength = str;
+        super.Dexterity = dex;
+        super.Inteligence = intel;
+        setMaxHealth(20);
+        setCurrenthealth(getMaxHealth());
+        maxmagic=maxHealth+(Inteligence/3);
+        currentmagic=maxmagic;
+    }
+    public int castChargedShot(int x){
+        if(currentmagic >= 5){
+            currentmagic -=5;
+            return attack(x)+ (Dexterity/2);}
+       else
+            return 0;
+    }
+    public int castHeal(int x){
+        if(currentmagic>=8){
+            currentmagic-=8;
+            return x+(Inteligence/2);
+        }
+        else
+            return 0;
+    }
+    public int getMaxMagic(){
+        return maxmagic;
+    }
+    public int getCurrentMagic(){
+        return currentmagic;
+    }
+}
---
+++ Rpg/Wizard.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rpg;
+
+
+public class Wizard extends Character{
+    private int maxmagic, currentmagic;
+    public Wizard(String name, int str, int dex, int intel){
+        super.Name = name;
+        super.Strength = str;
+        super.Dexterity = dex;
+        super.Inteligence = intel;
+        setMaxHealth(20);
+        setCurrenthealth(getMaxHealth());
+        maxmagic=maxHealth+(Inteligence/3);
+        currentmagic=maxmagic;
+    }
+    public int castLightningBolt(int x){
+        if(currentmagic >= 5){
+            currentmagic -=5;
+            return attack(x)+ (Inteligence/2);}
+       else
+            return 0;
+    }
+    public int castHeal(int x){
+        if(currentmagic>=8){
+            currentmagic-=8;
+            return x+(Inteligence/2);
+        }
+        else
+            return 0;
+    }
+    public int getMaxMagic(){
+        return maxmagic;
+    }
+    public int getCurrentMagic(){
+        return currentmagic;
+    }
+}
